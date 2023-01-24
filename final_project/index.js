aa5ba2b13fd272ab0fc6c55d06a2e5fb488a1bca
jiann1300 <jiannweykoh@gmail.com>
2023-01-24 10:50:44 -0500

    bug unfix

--- final_project/index.js
+++ final_project/index.js
@@ -3,30 +3,29 @@ const jwt = require('jsonwebtoken');
 const session = require('express-session')
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
+const users = require('./router/auth_users.js').users;
+const doesExist = require('./router/auth_users.js').isValid;
+// const doesExist = (username)=>{
+//   let userswithsamename = users.filter((user)=>{
+//     return user.username === username
+//   });
+//   if(userswithsamename.length > 0){
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
 
-let users = []
-
-const doesExist = (username)=>{
-  let userswithsamename = users.filter((user)=>{
-    return user.username === username
-  });
-  if(userswithsamename.length > 0){
-    return true;
-  } else {
-    return false;
-  }
-}
-
-const authenticatedUser = (username,password)=>{
-  let validusers = users.filter((user)=>{
-    return (user.username === username && user.password === password)
-  });
-  if(validusers.length > 0){
-    return true;
-  } else {
-    return false;
-  }
-}
+// const authenticatedUser = (username,password)=>{
+//   let validusers = users.filter((user)=>{
+//     return (user.username === username && user.password === password)
+//   });
+//   if(validusers.length > 0){
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
 
 const app = express();
 
@@ -36,44 +35,23 @@ app.use(express.json());
 
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
-app.use("/customer/auth/*", function auth(req,res,next){
-  if(req.session.authorization) {
-    token = req.session.authorization['accessToken'];
-    jwt.verify(token, "access",(err,user)=>{
-      if(!err){
-        req.user = user;
-        next();
-      }
-      else{
-        return res.status(403).json({message: "User not authenticated"})
-      }
-    });
-  } else {
-    return res.status(403).json({message: "User not logged in"})
-  }
-});
+// app.use("/customer/auth/*", function auth(req,res,next){
+//   if(req.session.authorization) {
+//     token = req.session.authorization['accessToken'];
+//     jwt.verify(token, "access",(err,user)=>{
+//       if(!err){
+//         req.user = user;
+//         next();
+//       }
+//       else{
+//         return res.status(403).json({message: "User not authenticated"})
+//       }
+//     });
+//   } else {
+//     return res.status(403).json({message: "User not logged in"})
+//   }
+// });
  
-app.post("/login", (req,res) => {
-  const username = req.body.username;
-  const password = req.body.password;
-
-  if (!username || !password) {
-      return res.status(404).json({message: "Error logging in"});
-  }
-
-  if (authenticatedUser(username,password)) {
-    let accessToken = jwt.sign({
-      data: password
-    }, 'access', { expiresIn: 60 });
-
-    req.session.authorization = {
-      accessToken,username
-  }
-  return res.status(200).send("User successfully logged in");
-  } else {
-    return res.status(208).json({message: "Invalid Login. Check username and password"});
-  }
-});
 
 app.post("/register", (req,res) => {
   const username = req.body.username;
@@ -89,10 +67,14 @@ app.post("/register", (req,res) => {
   } 
   return res.status(404).json({message: "Unable to register user."});
 });
+app.get('/a',function (req, res) {
+    //Write your code here
+    res.send(users)
+  });
 
 const PORT =5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT,()=>console.log("Server is running "+PORT));
